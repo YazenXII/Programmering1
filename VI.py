dbda7e373618ca6cc6e4afae309acd3d238c2104
Yazen Alshammaa <yazen.alshammaa@elev.ga.ntig.se>
2022-10-10 13:16:43 +0200

    VI

---
+++ VI.py
@@ -0,0 +1,68 @@
+# Övning 3 går ut på att implementera så många av nedanstående funktioner som möjligt
+# Ta bort pass och implementera funktionerna
+
+
+def best(name):
+    # TODO Skriv ut att namnet är bäst
+    # Ex: "Katherine" in - skriver ut "Katherine är bäst"
+    print(name, "är best")
+
+
+best("yazen")
+best("hampus")
+
+
+
+def square(number):
+    # TODO Returnera talet kvadrerat
+    # Ex: 5 in - 25 ut
+    print(number ** 99999)
+
+square(123)
+
+
+from II import C
+
+
+def sums(a, b):
+    # TODO Returnera summan av a och b
+    # Ex: 2, 6 in - 8 ut
+    print(a + b)
+
+sums(a = 6, b = 5)
+
+
+    
+
+
+# Nu blir det lite svårare
+
+
+def maximum(a, b, c):
+    # TODO Returnera det största av a,b,c
+    # Ex: 3, 6, 12 in - 12 ut
+    if c > a and a > b:
+     return c
+    elif a > b:
+     return C
+    else:
+     return b
+     
+
+print(maximum(33, 32, 55))
+
+
+def palindrom(ord):
+    # TODO Returnera True om ord är ett palindrom
+    # Ex: abba in - True ut
+    # Palindrom är ett ord som stavas likadant baklänges och framlänges.
+    
+    if 0 = -1 and 1 = -2 and 2 = -3 and 3 = -4
+
+palidrom("racecar")
+
+
+def prime(nr):
+    # TODO Returnera True om nr är ett primtal, annars returnera false
+    # Ex: 5 in - True ut
+    pass
